c304a3535ea1c16702a6d2e2fd701553156ae05b
r-s-coding <112776326+r-s-coding@users.noreply.github.com>
2023-05-25 19:38:11 +0000

    Added the dropdown autocomplete search

--- src/components/AutoComplete.jsx
+++ src/components/AutoComplete.jsx
@@ -1,5 +1,54 @@
+import { useState, useEffect } from 'react'
+import finnHub from '../apis/finnHub'
+
 export const AutoComplete = () => {
+  const [search, setSearch] = useState("")
+  const [results, setResults] = useState([])
+
+  const renderDropdown = () => {
+    const dropDownClass = search.length > 1 ? "show" : null
+    return (
+      <ul style={{height: "500px", overflowY: scroll, overflowX: "hidden", cursor: "pointer"}}className={`dropdown-menu ${dropDownClass}`}>
+        {results.map((result) => {
+          return (
+            <li key={result.symbol} className="dropdown-item"> {result.description} ({result.symbol})</li>
+          )
+        })}
+        </ul>
+    )
+  }
+
+  useEffect(() => {
+    let isMounted = true
+    const fetchData = async () => {
+      try {
+        const response = await finnHub.get("/search", {
+          params: {
+            q: search
+          }
+        })
+        if (isMounted) {
+          setResults(response.data.result)
+        }        
+      } catch (err) {
+        
+      }
+    }
+    if (search.length > 1) {
+      fetchData()
+    } else {
+      setResults([])
+    }
+    return () => (isMounted = false)
+  }, [search])
+  
   return (
-    <div> AutoComplete </div>
+    <div className="w-50 p-5 rounded mx-autp"> 
+      <div className="form-floating dropdown">
+        <input style={{backgroundColor: "rgba(145, 158, 171, 0.04)"}} id="search" type="text" className="form-control" placeholder="Search" autoComplete="off" value={search} onChange={(e) => setSearch(e.target.value)}></input>
+          <label htmlFor="search"> Search </label>
+        {renderDropdown()}
+        </div>
+    </div>
   )
 }
